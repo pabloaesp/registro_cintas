d61d56672d51b61075f55c756caefadf8d13b603
Pablo Espinoza <pabloaesp@gmail.com>
2022-11-06 15:04:45 -0300

    Registro de respaldo validado v1 listo

--- api/controllers/BackupRegisterController.js
+++ api/controllers/BackupRegisterController.js
@@ -24,38 +24,56 @@ function saveBackupRegister(req, res){
     // END.DATE* Se gestiona abajo
     backupData.status = true
 
-    BackupRegister.findOne({"backup": req.body.backup}, (err, backup) => {
+    BackupRegister.findOne({status: true}, (err, backup) => {
         if(err) return res.status(500).send({message: 'Error en la peticion'});
 
-        // LA MANERA DE VALIDAR SI SE PUEDE REGISTRAR UN BACKUP ES POR EL STATUS
-        // YA QUE HAY UNA SOLA CASETERA, POR LO TANTO SI HAY UN BACKUP EN TRUE NO SE PUEDE COLCAR OTRO
-        // QUE CAMBIE DE ESTADO CUANDO: POR MEDIO DE UN BOTON TERMINAR QUE SI SE HACE ANTES DEL TIEMPO
-        // DE GUARDADO TENGA CONFIRMACION O
-        // CUANDO SE CAMBIE EL LUGAR DEL REGISTRO DEL BACKUP 
+        if (backup) {
+            return res.status(500).send({message: 'Hay un respaldo en proceso!'})
 
+        }else{
+            // Asignando END_DATE* segun tipo de backup
+            Backup.findById({"_id": req.body.backup}, (err, data) => {
+                if(err) return res.status(500).send({message: 'Error en la peticion'});
+
+                if (data) {
+                    var hours = data.process_time; 
+                    // Seteando END_DATE*
+                    backupData.end_date = moment().add(hours, 'hours').unix();
+                    // var date = new Date(backup.end_date*1000);
+                
+                    // SALVO la data validada del registro del backup
+                    backupData.save((err, backupDataSaved) => {
+                        if(err) return res.status(500).send({message: 'Error al guardar el registro'});
+                
+                        if(!backupDataSaved) return res.status(404).send({message: 'No se ha guardado el registro'});
+                
+                        return res.status(200).send({backup: backupDataSaved});
+                    });
+                }
+            });
+        }
     });
-
-    // // Buscando datos del tipo de Backup para validar si no hay uno de igual tipo en curso y el END_DATE* 
-    // Backup.findById({"_id": req.body.backup}, (err, data) => {
-    //     if(err) return res.status(500).send({message: 'Error en la peticion'});
-
-    //     if (data) {
-    //         var hours = data.process_time; 
-    //         backupData.end_date = moment().add(hours, 'hours').unix(); // Seteando END_DATE*
-        
-    //         // SALVO la data validada del registro del backup
-    //         backupData.save((err, backupDataSaved) => {
-    //             if(err) return res.status(500).send({message: 'Error al guardar el registro'});
-        
-    //             if(!backupDataSaved) return res.status(404).send({message: 'No se ha guardado el registro'});
-        
-    //             return res.status(200).send({backup: backupDataSaved});
-    //         });
-    //     }
-    // });
-
 }
 
+
+function deleteBackupRegister(req, res){
+    var registerId = req.params.id;
+
+    Tape.findOne({'_id': registerId}, (err, register) => {
+        if(err) return res.status(500).send({message: 'Error en la peticion'});
+        
+        if(register == null) return res.status(404).send({message: 'El respaldo no existe.'});
+
+        if(tape){
+            Tape.deleteOne({'_id': tapeId}, (err, tapeRemoved) => {
+                if(err) return res.status(500).send({message: 'Error al borrar la cinta'});
+        
+                if (tapeRemoved) return res.status(200).send({message: 'Cinta eliminada correctamente.'});        
+            });
+        }
+    });
+    
+}
 
 
 module.exports = {
